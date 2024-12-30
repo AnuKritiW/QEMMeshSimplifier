390a094fbff0f28a633ef8dcaaa8fe121abb2e00
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2024-12-30 17:28:15 +0000

    Add button to simplify by 50 faces each time.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,18 @@
 #include "polyscope/surface_mesh.h"
 #include "QEMSimplifier.h"
 
+// Helper to re-extract the geometry from mesh and update Polyscope
+void rebuildPolyscopeMesh(TriMesh& _mesh)
+{
+    // Convert mesh to arrays
+    Eigen::MatrixXd vertices_matrix;
+    Eigen::MatrixXi faces_matrix;
+    Parser::extractMeshData(_mesh, vertices_matrix, faces_matrix);
+
+    polyscope::removeStructure("Surface Mesh");
+    polyscope::registerSurfaceMesh("Surface Mesh", vertices_matrix, faces_matrix);
+}
+
 int main() {
     TriMesh mesh;
     std::string filename = "object-files/gourd.obj";
@@ -17,12 +29,7 @@ int main() {
 
     std::cout << "Mesh loaded successfully!" << std::endl;
 
-    // TODO: add button to polyscope viewer which allows real time simplification
-    const unsigned int tgtNumFaces = 200;
-
     // Initialize the QEMSimplifier and simplify the mesh
-    QEMSimplifier simplifier;
-    simplifier.simplifyMesh(mesh, tgtNumFaces);
 
     // Create matrices to store vertices and faces
     Eigen::MatrixXd vertices_matrix;
@@ -33,7 +40,20 @@ int main() {
 
     // Polyscope viewer
     polyscope::init();
-    polyscope::registerSurfaceMesh("icosahedron", vertices_matrix, faces_matrix);
+    polyscope::registerSurfaceMesh("Surface Mesh", vertices_matrix, faces_matrix);
+
+    polyscope::state::userCallback = [&mesh]()
+    {
+        // Polyscope uses ImGui
+        if (ImGui::Button("Simplify Once"))
+        {
+            QEMSimplifier simplifier;
+            simplifier.simplifyMesh(mesh, mesh.n_faces() - 50); // remove 50 faces each time
+
+            rebuildPolyscopeMesh(mesh);
+        }
+    };
+
     polyscope::show();
     std::cout << "Polyscope viewer initialised!\n";
 
